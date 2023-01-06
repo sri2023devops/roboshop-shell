0216b658b8cc07f17df1fc05ada81f1dcfa4bdc2
ADMIN <sreeinbox@gmail.com>
2023-01-06 19:51:44 +0530

    robo shop mongodb script updated

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,9 @@ script_location=$(pwd)
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 yum install mongodb-org -y
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
+
