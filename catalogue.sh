deac6ba8557058e4f66e320f3ca8d1f90587e1c3
ADMIN <sreeinbox@gmail.com>
2023-01-07 19:34:08 +0530

    updated nginx.conf and catalague.service files

--- catalogue.sh
+++ catalogue.sh
@@ -22,4 +22,4 @@ systemctl start catalogue
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 
-mongo --host localhost </app/schema/catalogue.js
+mongo --host mongodb-dev.sreedevops.online </app/schema/catalogue.js
