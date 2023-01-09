66f1e9586a9aff4b68e4d610741d19f5f01d2219
ADMIN <sreeinbox@gmail.com>
2023-01-09 19:40:57 +0530

    common.sh

--- common.sh
+++ common.sh
@@ -1,18 +1,81 @@
 script_location=$(pwd)
 LOG=/tmp/roboshop.log
-status_check()
-{
+
+status_check() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
+    echo -e "\e[1;32mSUCCESS\e[0m"
   else
-    echo -e "\e[31mFAILURE\e[0m"
-    echo "Refer log for more details LOG- ${LOG}"
-  exit
+    echo -e "\e[1;31mFAILURE\e[0m"
+    echo "Refer Log file for more information, LOG - ${LOG}"
+    exit
   fi
 }
 
+print_head() {
+  echo -e "\e[1m $1 \e[0m"
+}
 
-print_head()
-{
-  echo -e "\e[1m  $1 \e[0m"
+NODEJS() {
+  print_head "Configuring NodeJS Repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
+  status_check
+
+  print_head "Install NodeJS"
+  yum install nodejs -y &>>${LOG}
+  status_check
+
+  print_head "Add Application User"
+  id roboshop &>>${LOG}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${LOG}
+  fi
+  status_check
+
+  mkdir -p /app &>>${LOG}
+
+  print_head "Downloading App content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${LOG}
+  status_check
+
+  print_head "Cleanup Old Content"
+  rm -rf /app/* &>>${LOG}
+  status_check
+
+  print_head "Extracting App Content"
+  cd /app
+  unzip /tmp/${component}.zip &>>${LOG}
+  status_check
+
+  print_head "Installing NodeJS Dependencies"
+  cd /app &>>${LOG}
+  npm install &>>${LOG}
+  status_check
+
+  print_head "Configuring ${component} Service File"
+  cp ${script_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${LOG}
+  status_check
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${LOG}
+  status_check
+
+  print_head "Enable ${component} Service "
+  systemctl enable ${component} &>>${LOG}
+  status_check
+
+  print_head "Start ${component} service "
+  systemctl start ${component} &>>${LOG}
+  status_check
+
+  print_head "Configuring Mongo Repo "
+  cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${LOG}
+  status_check
+
+  print_head "Install Mongo Client"
+  yum install mongodb-org-shell -y &>>${LOG}
+  status_check
+
+  print_head "Load Schema"
+  mongo --host mongodb-dev.devopsb70.online </app/schema/${component}.js &>>${LOG}
+  status_check
 }
